113b2619583736808ad49530bba3c3468efe3f94
Saad Rahim <saad.rahim@amd.com>
2019-08-27 14:58:04 -0700

    Adding field

--- .jenkins/Common.groovy
+++ .jenkins/Common.groovy
@@ -1,104 +1,104 @@
 import com.amd.project.*
 import com.amd.docker.*
 import java.nio.file.Path;
+ mport groovy.transform.Field
 
+@Field boolean formatCheck = false
 
-    boolean formatCheck = false
+@Field def compileCommand =
+{
+    platform, project->
 
-    def compileCommand =
+    project.paths.construct_build_prefix()
+    
+    def command 
+
+    if(platform.jenkinsLabel.contains('hip-clang'))
     {
-        platform, project->
-
-        project.paths.construct_build_prefix()
-        
-        def command 
-
-        if(platform.jenkinsLabel.contains('hip-clang'))
-        {
-            command = """#!/usr/bin/env bash
-                    set -x
-                    cd ${project.paths.project_build_prefix}
-                    LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=/opt/rocm/bin/hipcc ${project.paths.build_command} --hip-clang
-                    """
-        }
-        else
-        {
-            command = """#!/usr/bin/env bash
-                    set -x
-                    cd ${project.paths.project_build_prefix}
-                    LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=/opt/rocm/bin/hcc ${project.paths.build_command}
-                    """
-        }
-
-        platform.runCommand(this, command)
-    }
-
-    def testCommand =
-    {
-        platform, project->
-
-        def command
-
-        if(platform.jenkinsLabel.contains('centos'))
-        {
-            command = """#!/usr/bin/env bash
-                    set -x
-                    cd ${project.paths.project_build_prefix}/build/release
-                    make -j4
-                    sudo ctest --output-on-failure
+        command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=/opt/rocm/bin/hipcc ${project.paths.build_command} --hip-clang
                 """
-        }
-        else
-        {
-            command = """#!/usr/bin/env bash
-                    set -x
-                    cd ${project.paths.project_build_prefix}/build/release
-                    make -j4
-                    ctest --output-on-failure
-                """
-        }
-
-        platform.runCommand(this, command)
     }
-
-    def packageCommand =
+    else
     {
-        platform, project->
-
-        def command
-        
-        if(platform.jenkinsLabel.contains('centos'))
-        {
-            command = """
-                    set -x
-                    cd ${project.paths.project_build_prefix}/build/release
-                    make package
-                    rm -rf package && mkdir -p package
-                    mv *.rpm package/
-                    rpm -qlp package/*.rpm
-                  """
-            
-            platform.runCommand(this, command)
-            platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/release/package/*.rpm""")
-        }
-        else if(platform.jenkinsLabel.contains('hip-clang'))
-        {
-            packageCommand = null
-        }
-        else
-        {
-            command = """
-                    set -x
-                    cd ${project.paths.project_build_prefix}/build/release
-                    make package
-                    rm -rf package && mkdir -p package
-                    mv *.deb package/
-                    dpkg -c package/*.deb
-                  """        
-            
-            platform.runCommand(this, command)
-            platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/release/package/*.deb""")
-        }
+        command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=/opt/rocm/bin/hcc ${project.paths.build_command}
+                """
     }
+
+    platform.runCommand(this, command)
+}
+
+@Field def testCommand =
+{
+    platform, project->
+
+    def command
+
+    if(platform.jenkinsLabel.contains('centos'))
+    {
+        command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}/build/release
+                make -j4
+                sudo ctest --output-on-failure
+            """
+    }
+    else
+    {
+        command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}/build/release
+                make -j4
+                ctest --output-on-failure
+            """
+    }
+
+    platform.runCommand(this, command)
+}
+
+@Field def packageCommand =
+{
+    platform, project->
+
+    def command
+    
+    if(platform.jenkinsLabel.contains('centos'))
+    {
+        command = """
+                set -x
+                cd ${project.paths.project_build_prefix}/build/release
+                make package
+                rm -rf package && mkdir -p package
+                mv *.rpm package/
+                rpm -qlp package/*.rpm
+              """
+        
+        platform.runCommand(this, command)
+        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/release/package/*.rpm""")
+    }
+    else if(platform.jenkinsLabel.contains('hip-clang'))
+    {
+        packageCommand = null
+    }
+    else
+    {
+        command = """
+                set -x
+                cd ${project.paths.project_build_prefix}/build/release
+                make package
+                rm -rf package && mkdir -p package
+                mv *.deb package/
+                dpkg -c package/*.deb
+              """        
+        
+        platform.runCommand(this, command)
+        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/release/package/*.deb""")
+    }
+}
 
 return this
