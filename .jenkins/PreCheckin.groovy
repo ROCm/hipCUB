3cf12c981219719b9f754f8793b5320a74fa3963
Eiden Yoshida <eiden.yoshida@amd.com>
2019-11-26 12:27:57 -0800

    Try loading from source folder

--- .jenkins/PreCheckin.groovy
+++ .jenkins/PreCheckin.groovy
@@ -24,13 +24,14 @@ hipCUBCI:
     def compileCommand =
     {
         platform, project->
-        sh '''pwd
-              ls'''
+        // sh '''pwd
+        //       ls'''
         // Needed to get the latest version of Common.groovy. To be properly implemented in the future in rocJenkins
-        checkout scm
+        // checkout scm
         sh '''pwd
-              ls'''
-        commonGroovy = load ".jenkins/Common.groovy"
+              ls
+              ls ${project.paths.project_src_prefix} -a'''
+        commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/Common.groovy"
         commonGroovy.runCompileCommand(platform, project)
     }
 
