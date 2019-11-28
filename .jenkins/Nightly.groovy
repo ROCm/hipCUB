cf8a13e41b7aac290e1fe927055545bcb83ccf73
Saad Rahim <saad.rahim@amd.com>
2019-11-28 13:36:15 -0800

    Testing nightly

---
+++ .jenkins/Nightly.groovy
@@ -0,0 +1,65 @@
+#!/usr/bin/env groovy
+// This shared library is available at https://github.com/ROCmSoftwarePlatform/rocJENKINS/
+@Library('rocJenkins') _
+
+// This file is for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+import com.amd.project.*
+import com.amd.docker.*
+import java.nio.file.Path;
+
+
+////////////////////////////////////////////////////////////////////////
+// Mostly generated from snippet generator 'properties; set job properties'
+// Time-based triggers added to execute nightly tests, eg '30 2 * * *' means 2:30 AM
+properties([
+    pipelineTriggers([cron('0 1 * * *'), [$class: 'PeriodicFolderTrigger', interval: '5m']]),
+        buildDiscarder(logRotator(
+	      artifactDaysToKeepStr: '',
+	            artifactNumToKeepStr: '',
+		          daysToKeepStr: '',
+			        numToKeepStr: '10')),
+				    disableConcurrentBuilds(),
+				        [$class: 'CopyArtifactPermissionProperty', projectNames: '*']
+					   ])
+
+
+
+hipCUBCI:
+{
+
+    def hipcub = new rocProject('hipCUB')
+
+    // Define test architectures, optional rocm version argument is available
+    def nodes = new dockerNodes(['ubuntu && gfx803', 'sles && gfx906', 'gfx900 && centos7', 'gfx906 && centos7'], hipcub)
+
+    boolean formatCheck = false
+
+    def commonGroovy
+
+    def compileCommand =
+    {
+        platform, project->
+  
+        commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/Common.groovy"
+        commonGroovy.runCompileCommand(platform, project)
+    }
+
+    def testCommand =
+    {
+        platform, project->
+
+        commonGroovy.runTestCommand(platform, project)
+    }
+
+    def packageCommand =
+    {
+        platform, project->
+        
+        commonGroovy.runPackageCommand(platform, project)
+    }
+
+    buildProject(hipcub, formatCheck, nodes.dockerArray, compileCommand, testCommand, packageCommand)
+}
+
