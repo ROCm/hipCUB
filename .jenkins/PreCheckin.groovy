ecb07f6444fac354d6a975a485014a88db479b1c
Eiden Yoshida <eiden.yoshida@amd.com>
2019-12-02 10:37:00 -0700

    Put Common.groovy in a stage so it doesn't show the hash

--- .jenkins/PreCheckin.groovy
+++ .jenkins/PreCheckin.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 // This shared library is available at https://github.com/ROCmSoftwarePlatform/rocJENKINS/
-@Library('rocJenkins') _
+@Library('rocJenkins@addBuildName') _
 
 // This file is for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
@@ -11,8 +11,8 @@ import java.nio.file.Path;
 
 hipCUBCI:
 {
-
-    def hipcub = new rocProject('hipCUB', 'PreCheckin')
+    def buildName = 'PreCheckin'
+    def hipcub = new rocProject('hipCUB', buildName)
 
     // Define test architectures, optional rocm version argument is available
     def nodes = new dockerNodes(['ubuntu && gfx803', 'sles && gfx906', 'gfx900 && centos7', 'gfx906 && centos7'], hipcub)
@@ -25,7 +25,9 @@ hipCUBCI:
     {
         platform, project->
   
-        commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/Common.groovy"
+        stage(buildName + ' Initialize CI ' + platform.jenkinsLabel) {
+            commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/Common.groovy"
+        }
         commonGroovy.runCompileCommand(platform, project)
     }
 
