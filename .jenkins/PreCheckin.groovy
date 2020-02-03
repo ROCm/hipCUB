155374ed68e873db811bfdeb03e1892ccff5d096
Eiden Yoshida <eiden.yoshida@amd.com>
2020-02-03 16:43:46 -0700

    Test iterating through configurations

--- .jenkins/PreCheckin.groovy
+++ .jenkins/PreCheckin.groovy
@@ -54,11 +54,14 @@ ci: {
     echo buildURL
     println(buildURL)
     // 'http://10.216.151.18:8080/job/PreCheckin/job/Tensile/job/develop/18'
-
-    def nodeDetails = [ubuntu16:['gfx803'],centos7:['gfx803','gfx900'],sles15sp1:['gfx803']]
-    def jobName = 'compute-rocm-dkms-no-npi'
-    
-    hipCUBCI(nodeDetails, jobName)
+    def jobNameList = [compute-rocm-dkms-no-npi: [ubuntu16:['gfx803'],centos7:['gfx803','gfx900'],sles15sp1:['gfx803']], 
+                        rocm-docker: [ubuntu16:['gfx803'],centos7:['gfx803','gfx900'],sles15sp1:['gfx803']]]
+    jobNameList.each 
+    {
+        jobName, nodeDetails->
+        if (env.buildURL.contains(jobName))
+            hipCUBCI(nodeDetails, jobName)
+    }
 }
 
 
