c7b57a467aacb9e16110925e1097d3b21a292843
Eiden Yoshida <eiden.yoshida@amd.com>
2020-02-03 17:16:54 -0700

    blah

--- .jenkins/PreCheckin.groovy
+++ .jenkins/PreCheckin.groovy
@@ -9,6 +9,29 @@ import com.amd.project.*
 import com.amd.docker.*
 import java.nio.file.Path;
 
+ci: { 
+    String buildURL = env.BUILD_URL
+
+    def propertyList = ["compute-rocm-dkms-no-npi":[pipelineTriggers([cron('0 1 * * 0')])], 
+                        "rocm-docker":[]]
+    def jobNameList = ["compute-rocm-dkms-no-npi":([ubuntu16:['gfx803'],centos7:['gfx803','gfx900'],sles15sp1:['gfx803']]), 
+                        "rocm-docker":([ubuntu16:['gfx803'],centos7:['gfx803','gfx900'],sles15sp1:['gfx803']])]
+
+    propertyList.each 
+    {
+        jobName, property->
+        if (buildURL.contains(jobName))
+            properties(auxiliary.setProperties(property))
+    }
+
+    jobNameList.each 
+    {
+        jobName, nodeDetails->
+        echo jobName
+        if (buildURL.contains(jobName))
+            hipCUBCI(nodeDetails, jobName)
+    }
+}
 
 def hipCUBCI = 
 {
@@ -46,29 +69,6 @@ def hipCUBCI =
     }
 
     buildProject(hipcub, formatCheck, nodes.dockerArray, compileCommand, testCommand, packageCommand)
-}
-
-ci: { 
-    String buildURL = env.BUILD_URL
-    def jobNameList = ["compute-rocm-dkms-no-npi":([ubuntu16:['gfx803'],centos7:['gfx803','gfx900'],sles15sp1:['gfx803']]), 
-                        "rocm-docker":([ubuntu16:['gfx803'],centos7:['gfx803','gfx900'],sles15sp1:['gfx803']])]
-
-    def propertyList = ["compute-rocm-dkms-no-npi":[pipelineTriggers([cron('0 1 * * 0')])], 
-                        "rocm-docker":[]]
-
-    propertyList.each 
-    {
-        jobName, property->
-        if (buildURL.contains(jobName))
-            properties(auxiliary.setProperties(property))
-    }
-    jobNameList.each 
-    {
-        jobName, nodeDetails->
-        echo jobName
-        if (buildURL.contains(jobName))
-            hipCUBCI(nodeDetails, jobName)
-    }
 }
 
 
