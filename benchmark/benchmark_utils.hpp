9a06de19e9c06ddf97e9c9431e7241db41383a89
Anna Kruseman <anna@streamhpc.com>
2020-05-01 14:04:54 +0200

    Add benchmark for block_radix_sort
    [ROCm/hipCUB commit: e9e44647c71580c86a057987072ad1fbe38f190f]

--- benchmark/benchmark_utils.hpp
+++ benchmark/benchmark_utils.hpp
@@ -29,5 +29,156 @@
 // hipCUB
 #include <hipcub/hipcub.hpp>
 
+namespace benchmark_utils
+{
+
+// get_random_data() generates only part of sequence and replicates it,
+// because benchmarks usually do not need "true" random sequence.
+template<class T>
+inline auto get_random_data(size_t size, T min, T max, size_t max_random_size = 1024 * 1024)
+    -> typename std::enable_if<std::is_integral<T>::value, std::vector<T>>::type
+{
+    std::random_device rd;
+    std::default_random_engine gen(rd());
+    std::uniform_int_distribution<T> distribution(min, max);
+    std::vector<T> data(size);
+    std::generate(
+        data.begin(), data.begin() + std::min(size, max_random_size),
+        [&]() { return distribution(gen); }
+    );
+    for(size_t i = max_random_size; i < size; i += max_random_size)
+    {
+        std::copy_n(data.begin(), std::min(size - i, max_random_size), data.begin() + i);
+    }
+    return data;
+}
+
+template<class T>
+inline auto get_random_data(size_t size, T min, T max, size_t max_random_size = 1024 * 1024)
+    -> typename std::enable_if<std::is_floating_point<T>::value, std::vector<T>>::type
+{
+    std::random_device rd;
+    std::default_random_engine gen(rd());
+    //using dis_type = typename std::conditional<std::is_same<rocprim::half, T>::value, float, T>::type;
+    std::uniform_real_distribution<T> distribution(min, max);
+    std::vector<T> data(size);
+    std::generate(
+        data.begin(), data.begin() + std::min(size, max_random_size),
+        [&]() { return distribution(gen); }
+    );
+    for(size_t i = max_random_size; i < size; i += max_random_size)
+    {
+        std::copy_n(data.begin(), std::min(size - i, max_random_size), data.begin() + i);
+    }
+    return data;
+}
+
+template<class T>
+inline std::vector<T> get_random_data01(size_t size, float p, size_t max_random_size = 1024 * 1024)
+{
+    std::random_device rd;
+    std::default_random_engine gen(rd());
+    std::bernoulli_distribution distribution(p);
+    std::vector<T> data(size);
+    std::generate(
+        data.begin(), data.begin() + std::min(size, max_random_size),
+        [&]() { return distribution(gen); }
+    );
+    for(size_t i = max_random_size; i < size; i += max_random_size)
+    {
+        std::copy_n(data.begin(), std::min(size - i, max_random_size), data.begin() + i);
+    }
+    return data;
+}
+
+template<class T>
+inline T get_random_value(T min, T max)
+{
+    return get_random_data(1, min, max)[0];
+}
+
+// template<class T, class U = T>
+// struct custom_type
+// {
+//     using first_type = T;
+//     using second_type = U;
+
+//     T x;
+//     U y;
+
+//     ROCPRIM_HOST_DEVICE inline
+//     custom_type(T xx = 0, U yy = 0) : x(xx), y(yy)
+//     {
+//     }
+
+//     ROCPRIM_HOST_DEVICE inline
+//     ~custom_type() = default;
+
+//     ROCPRIM_HOST_DEVICE inline
+//     custom_type operator+(const custom_type& rhs) const
+//     {
+//         return custom_type(x + rhs.x, y + rhs.y);
+//     }
+
+//     ROCPRIM_HOST_DEVICE inline
+//     bool operator<(const custom_type& rhs) const
+//     {
+//         return (x < rhs.x || (x == rhs.x && y < rhs.y));
+//     }
+
+//     ROCPRIM_HOST_DEVICE inline
+//     bool operator==(const custom_type& rhs) const
+//     {
+//         return x == rhs.x && y == rhs.y;
+//     }
+// };
+
+// template<typename>
+// struct is_custom_type : std::false_type {};
+
+// template<class T, class U>
+// struct is_custom_type<custom_type<T,U>> : std::true_type {};
+
+// template<class T>
+// inline auto get_random_data(size_t size, T min, T max, size_t max_random_size = 1024 * 1024)
+//     -> typename std::enable_if<is_custom_type<T>::value, std::vector<T>>::type
+// {
+//     using first_type = typename T::first_type;
+//     using second_type = typename T::second_type;
+//     std::vector<T> data(size);
+//     auto fdata = get_random_data<first_type>(size, min.x, max.x, max_random_size);
+//     auto sdata = get_random_data<second_type>(size, min.y, max.y, max_random_size);
+//     for(size_t i = 0; i < size; i++)
+//     {
+//         data[i] = T(fdata[i], sdata[i]);
+//     }
+//     return data;
+// }
+
+// template<class T>
+// inline auto get_random_data(size_t size, T min, T max, size_t max_random_size = 1024 * 1024)
+//     -> typename std::enable_if<!is_custom_type<T>::value && !std::is_same<decltype(max.x), void>::value, std::vector<T>>::type
+// {
+//     // NOTE 1: post-increment operator required, because HIP has different typedefs for vector field types
+//     //         when using HCC or HIP-Clang. Using HIP-Clang members are accessed as fields of a struct via
+//     //         a union, but in HCC mode they are proxy types (Scalar_accessor). STL algorithms don't
+//     //         always tolerate proxies. Unfortunately, Scalar_accessor doesn't have any member typedefs to
+//     //         conveniently obtain the inner stored type. All operations on it (operator+, operator+=,
+//     //         CTOR, etc.) return a reference to an accessor, it is only the post-increment operator that
+//     //         returns a copy of the stored type, hence we take the decltype of that.
+//     //
+//     // NOTE 2: decltype() is unevaluated context. We don't really modify max, just compute the type of the
+//     //         expression if we were to actually call it.
+//     using field_type = decltype(max.x++);
+//     std::vector<T> data(size);
+//     auto field_data = get_random_data<field_type>(size, min.x, max.x, max_random_size);
+//     for(size_t i = 0; i < size; i++)
+//     {
+//         data[i] = T(field_data[i]);
+//     }
+//     return data;
+// }
+
+} // end benchmark_util namespace
 
 #endif // ROCPRIM_BENCHMARK_UTILS_HPP_
