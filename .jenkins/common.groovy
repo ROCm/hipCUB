f0a23affd6913999c5c83bd1bc4babd590fb1a76
Saad Rahim <saad.rahim@amd.com>
2020-07-29 15:26:38 -0700

    sameorg

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -1,7 +1,7 @@
 // This file is for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
 
-def runCompileCommand(platform, project, jobName, boolean debug=false, boolean sameOrg=false)
+def runCompileCommand(platform, project, jobName, boolean debug=false, boolean sameOrg=true)
 {
     project.paths.construct_build_prefix()
         
