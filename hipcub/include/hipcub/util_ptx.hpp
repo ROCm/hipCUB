a52bee47249c66194e929dd85d139cf7a4296057
Istvan Kiss <istvan@streamhpc.com>
2021-04-20 18:56:43 +0200

    Add block radix rank test and fix it

--- hipcub/include/hipcub/util_ptx.hpp
+++ hipcub/include/hipcub/util_ptx.hpp
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Copyright (c) 2010-2011, Duane Merrill.  All rights reserved.
+ * Copyright (c) 2011, Duane Merrill.  All rights reserved.
  * Copyright (c) 2011-2018, NVIDIA CORPORATION.  All rights reserved.
- * Modifications Copyright (c) 2020, Advanced Micro Devices, Inc.  All rights reserved.
+ * Modifications Copyright (c) 2021, Advanced Micro Devices, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -27,18 +27,14 @@
  *
  ******************************************************************************/
 
-#ifndef HIPCUB_BLOCK_BLOCK_RADIX_RANK_HPP_
-#define HIPCUB_BLOCK_BLOCK_RADIX_RANK_HPP_
-
-#include "block_scan.hpp"
-#include "../thread/thread_scan.hpp"
-#include "../thread/thread_reduce.hpp"
+#ifndef HIPCUB_UTIL_PTX_HPP_
+#define HIPCUB_UTIL_PTX_HPP_
 
 #ifdef __HIP_PLATFORM_HCC__
-    #include "../backend/rocprim/block/block_radix_rank.hpp"
+    #include "backend/rocprim/util_ptx.hpp"
 #elif defined(__HIP_PLATFORM_NVCC__)
-    #include "../config.hpp"
-    #include <cub/block/block_radix_rank.cuh>
+    #include <cub/util_ptx.cuh>
 #endif
 
-#endif // HIPCUB_BLOCK_BLOCK_RADIX_RANK_HPP_
+
+#endif
