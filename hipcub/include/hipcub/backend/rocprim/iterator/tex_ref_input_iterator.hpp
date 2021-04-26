f30bdf3e2d2e3ad374642faf6c17e7c5d9b85dd2
Stanley Tsang <stanley.tsang@amd.com>
2021-04-26 17:01:08 -0600

    Cherry picking update iterators commit to fix build error in frameworks

--- hipcub/include/hipcub/backend/rocprim/iterator/tex_ref_input_iterator.hpp
+++ hipcub/include/hipcub/backend/rocprim/iterator/tex_ref_input_iterator.hpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2010-2011, Duane Merrill.  All rights reserved.
+ * Copyright (c) 2011, Duane Merrill.  All rights reserved.
  * Copyright (c) 2011-2018, NVIDIA CORPORATION.  All rights reserved.
  * Modifications Copyright (c) 2017-2021, Advanced Micro Devices, Inc.  All rights reserved.
  *
@@ -30,7 +30,15 @@
 #ifndef HIPCUB_ROCPRIM_ITERATOR_TEX_REF_INPUT_ITERATOR_HPP_
 #define HIPCUB_ROCPRIM_ITERATOR_TEX_REF_INPUT_ITERATOR_HPP_
 
+#include <iterator>
+#include <iostream>
+
 #include "../../../config.hpp"
+
+#if (THRUST_VERSION >= 100700)    // This iterator is compatible with Thrust API 1.7 and newer
+    #include <thrust/iterator/iterator_facade.h>
+    #include <thrust/iterator/iterator_traits.h>
+#endif // THRUST_VERSION
 
 #include <rocprim/iterator/texture_cache_iterator.hpp>
 
@@ -77,4 +85,3 @@ class TexRefInputIterator : public ::rocprim::texture_cache_iterator<T, OffsetT>
 END_HIPCUB_NAMESPACE
 
 #endif // HIPCUB_ROCPRIM_ITERATOR_TEX_OBJ_INPUT_ITERATOR_HPP_
-
