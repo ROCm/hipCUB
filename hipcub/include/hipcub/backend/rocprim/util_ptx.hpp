f24a7053474daf39707d4077ded481ccb25628f1
Istvan Kiss <istvan@streamhpc.com>
2021-05-18 12:15:35 +0200

    Put back original HIPCUB_WARP_THREADS with deprecated attribute
    Update gitlab CI
    
    
    [ROCm/hipCUB commit: cfde7828b5093f7f8d6e396ecf74e834404aa504]

--- hipcub/include/hipcub/backend/rocprim/util_ptx.hpp
+++ hipcub/include/hipcub/backend/rocprim/util_ptx.hpp
@@ -37,6 +37,7 @@
 
 #include <rocprim/intrinsics/warp_shuffle.hpp>
 
+#define HIPCUB_WARP_THREADS ::rocprim::warp_size()
 #define HIPCUB_DEVICE_WARP_THREADS ::rocprim::device_warp_size()
 #define HIPCUB_HOST_WARP_THREADS ::rocprim::host_warp_size()
 #define HIPCUB_ARCH 1 // ignored with rocPRIM backend
