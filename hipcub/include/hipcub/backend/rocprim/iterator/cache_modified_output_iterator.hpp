3da4fc777e51226bec71adc192e5c31670faadbc
Istvan Kiss <istvan@streamhpc.com>
2021-08-30 19:29:50 +0200

    Fix documentation in cache iterators

--- hipcub/include/hipcub/backend/rocprim/iterator/cache_modified_output_iterator.hpp
+++ hipcub/include/hipcub/backend/rocprim/iterator/cache_modified_output_iterator.hpp
@@ -173,12 +173,16 @@ public:
         return (ptr != rhs.ptr);
     }
 
+#ifndef DOXYGEN_SHOULD_SKIP_THIS    // Do not document
+
     /// ostream operator
     friend std::ostream& operator<<(std::ostream& os, const self_type& itr)
     {
         (void)itr;
         return os;
     }
+
+#endif
 };
 
 END_HIPCUB_NAMESPACE
