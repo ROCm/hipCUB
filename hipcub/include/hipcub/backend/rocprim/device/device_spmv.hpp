4791ffbefc5fcc92680dddcd223520031a5f5450
Stanley Tsang <stanley.tsang@amd.com>
2021-10-08 10:21:57 -0600

    Replacing hipPeekAtLastError with GetLastError (#164)
    [ROCm/hipCUB commit: 941db3cb669b4f67ae3022bbc497cc13b396423c]

--- hipcub/include/hipcub/backend/rocprim/device/device_spmv.hpp
+++ hipcub/include/hipcub/backend/rocprim/device/device_spmv.hpp
@@ -139,7 +139,7 @@ template <typename ValueT>
             size_t block_size = min(num_cols, DeviceSpmv::CsrMVKernel_MaxThreads);
             size_t grid_size = num_rows;
             CsrMVKernel<<<grid_size, block_size, 0, stream>>>(spmv_params);
-            status = hipPeekAtLastError();
+            status = hipGetLastError();
         }
         return status;
     }
