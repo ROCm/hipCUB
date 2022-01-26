3d0ed35841f83614c800da1adeec2638e9237909
Vince <vince@streamhpc.com>
2022-01-26 12:04:54 +0100

    `hipcub::WarpExchange` interface to `::rocprim::warp_exchange`

--- hipcub/include/hipcub/backend/rocprim/warp/warp_exchange.hpp
+++ hipcub/include/hipcub/backend/rocprim/warp/warp_exchange.hpp
@@ -33,6 +33,8 @@
 #include "../../../config.hpp"
 #include "../util_type.hpp"
 
+#include <rocprim/warp/warp_exchange.hpp>
+
 BEGIN_HIPCUB_NAMESPACE
 
 template <
@@ -41,33 +43,13 @@ template <
     int LOGICAL_WARP_THREADS = HIPCUB_DEVICE_WARP_THREADS,
     int ARCH = HIPCUB_ARCH
 >
-class WarpExchange
+class WarpExchange : private ::rocprim::warp_exchange<InputT, ITEMS_PER_THREAD, LOGICAL_WARP_THREADS>
 {
-    static_assert(PowerOfTwo<LOGICAL_WARP_THREADS>::VALUE,
-        "LOGICAL_WARP_THREADS must be a power of two");
-    
-    constexpr static int SMEM_BANKS = ::rocprim::detail::get_lds_banks_no();
-
-    constexpr static bool HAS_BANK_CONFLICTS =
-        ITEMS_PER_THREAD > 4 && PowerOfTwo<ITEMS_PER_THREAD>::VALUE;
-
-    constexpr static int BANK_CONFLICTS_PADDING =
-        HAS_BANK_CONFLICTS ? (ITEMS_PER_THREAD / SMEM_BANKS) : 0;
-
-    constexpr static int ITEMS_PER_TILE =
-        ITEMS_PER_THREAD * LOGICAL_WARP_THREADS + BANK_CONFLICTS_PADDING;
-
-    constexpr static bool IS_ARCH_WARP = LOGICAL_WARP_THREADS ==
-                                         HIPCUB_DEVICE_WARP_THREADS;
-
-    union _TempStorage
-    {
-        InputT items_shared[ITEMS_PER_TILE];
-    };
+    using base_type = typename rocprim::warp_exchange<InputT, ITEMS_PER_THREAD, LOGICAL_WARP_THREADS>;
+    using _TempStorage = typename base_type::storage_type;
 
     _TempStorage &temp_storage;
-    unsigned lane_id;
-   
+
 public:
     struct TempStorage : Uninitialized<_TempStorage> {};
 
@@ -75,8 +57,7 @@ public:
 
     explicit HIPCUB_DEVICE __forceinline__
     WarpExchange(TempStorage &temp_storage) :
-        temp_storage(temp_storage.Alias()),
-        lane_id(IS_ARCH_WARP ? LaneId() : LaneId() % LOGICAL_WARP_THREADS)
+        temp_storage(temp_storage.Alias())
     {
     }
 
@@ -86,20 +67,7 @@ public:
         const InputT (&input_items)[ITEMS_PER_THREAD],
         OutputT (&output_items)[ITEMS_PER_THREAD])
     {
-        for (int item = 0; item < ITEMS_PER_THREAD; ++item)
-        {
-            const int idx = ITEMS_PER_THREAD * lane_id + item;
-            temp_storage.items_shared[idx] = input_items[item];
-        }
-
-        // member mask is unused in rocPRIM
-        WARP_SYNC(0);
-
-        for (int item = 0; item < ITEMS_PER_THREAD; ++item)
-        {
-            const int idx = LOGICAL_WARP_THREADS * item + lane_id;
-            output_items[item] = temp_storage.items_shared[idx];
-        }
+        base_type::blocked_to_striped(input_items, output_items, temp_storage);
     }
 
     template <typename OutputT>
@@ -108,20 +76,7 @@ public:
         const InputT (&input_items)[ITEMS_PER_THREAD],
         OutputT (&output_items)[ITEMS_PER_THREAD])
     {
-        for (int item = 0; item < ITEMS_PER_THREAD; ++item)
-        {
-            const int idx = LOGICAL_WARP_THREADS * item + lane_id;
-            temp_storage.items_shared[idx] = input_items[item];
-        }
-
-        // member mask is unused in rocPRIM
-        WARP_SYNC(0);
-
-        for (int item = 0; item < ITEMS_PER_THREAD; ++item)
-        {
-            const int idx = ITEMS_PER_THREAD * lane_id + item;
-            output_items[item] = temp_storage.items_shared[idx];
-        }
+        base_type::striped_to_blocked(input_items, output_items, temp_storage);
     }
 
     template <typename OffsetT>
@@ -141,21 +96,7 @@ public:
         OutputT (&output_items)[ITEMS_PER_THREAD],
         OffsetT (&ranks)[ITEMS_PER_THREAD])
     {
-        ROCPRIM_UNROLL
-        for (int item = 0; item < ITEMS_PER_THREAD; ++item)
-        {
-            temp_storage.items_shared[ranks[item]] = input_items[item];
-        }
-
-        // member mask is unused in rocPRIM
-        WARP_SYNC(0);
-        
-        ROCPRIM_UNROLL
-        for (int item = 0; item < ITEMS_PER_THREAD; item++)
-        {
-            int item_offset = (item * LOGICAL_WARP_THREADS) + lane_id;
-            output_items[item] = temp_storage.items_shared[item_offset];
-        }
+        base_type::scatter_to_striped(input_items, output_items, ranks, temp_storage);
     }
 };
 
