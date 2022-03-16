fefd28aa0453fbfb28a0035574b9a35a840e63f5
Théo Battrel <theo@streamhpc.com>
2022-03-16 09:58:43 +0000

    Add the macros to ignore warning in config.hpp and remove it from block_adjacent_difference file and the from the tests

--- hipcub/include/hipcub/backend/rocprim/block/block_adjacent_difference.hpp
+++ hipcub/include/hipcub/backend/rocprim/block/block_adjacent_difference.hpp
@@ -34,21 +34,6 @@
 
 #include <rocprim/block/block_adjacent_difference.hpp>
 
-// Helper macros to disable warnings in clang
-#ifdef __clang__
-#define HIPCUB_PRAGMA_TO_STR(x) _Pragma(#x)
-#define HIPCUB_CLANG_SUPPRESS_WARNING_PUSH _Pragma("clang diagnostic push")
-#define HIPCUB_CLANG_SUPPRESS_WARNING(w) HIPCUB_PRAGMA_TO_STR(clang diagnostic ignored w)
-#define HIPCUB_CLANG_SUPPRESS_WARNING_POP _Pragma("clang diagnostic pop")
-#define HIPCUB_CLANG_SUPPRESS_WARNING_WITH_PUSH(w) \
-    HIPCUB_CLANG_SUPPRESS_WARNING_PUSH HIPCUB_CLANG_SUPPRESS_WARNING(w)
-#else // __clang__
-#define HIPCUB_CLANG_SUPPRESS_WARNING_PUSH
-#define HIPCUB_CLANG_SUPPRESS_WARNING(w)
-#define HIPCUB_CLANG_SUPPRESS_WARNING_POP
-#define HIPCUB_CLANG_SUPPRESS_WARNING_WITH_PUSH(w)
-#endif // __clang__
-
 BEGIN_HIPCUB_NAMESPACE
 
 template<
