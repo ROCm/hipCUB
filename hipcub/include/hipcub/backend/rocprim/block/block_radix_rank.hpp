b51f131bf45fb96a63f9301ffe36e53cc5acaecb
Matthias Knorr <matthias@streamhpc.com>
2022-06-23 15:02:38 +0000

    Updated copyright year

--- hipcub/include/hipcub/backend/rocprim/block/block_radix_rank.hpp
+++ hipcub/include/hipcub/backend/rocprim/block/block_radix_rank.hpp
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Copyright (c) 2011, Duane Merrill.  All rights reserved.
  * Copyright (c) 2011-2018, NVIDIA CORPORATION.  All rights reserved.
- * Modifications Copyright (c) 2021, Advanced Micro Devices, Inc.  All rights reserved.
+ * Modifications Copyright (c) 2021-2022, Advanced Micro Devices, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
