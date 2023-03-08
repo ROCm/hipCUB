495b6d33221228f7960455dfe1db77e577498f22
Gergely Meszaros <gergely@streamhpc.com>
2023-03-08 07:08:37 +0000

    Apply shellcheck suggestions to check-copyright.sh
    - Quoted arguments to prevent splitting and globbing
    - use the modern `$(...)` form for command substitution.
    - use git-config's ability canonicalize options to a type
    No functional change intended.

--- scripts/copyright-date/check-copyright.sh
+++ scripts/copyright-date/check-copyright.sh
@@ -5,7 +5,7 @@ preamble="Copyright +\([cC]\) +"
 postamble=",? +Advanced +Micro +Devices, +Inc\. +All +rights +reserved\."
 # End of configuration
 
-function print_help { printf \
+print_help() { printf \
 "\033[36musuage\033[0m: \033[33mcheck_year.sh [-h] [-u] [-a] [-d <SHA>] [-k] [-v]\033[0m
 \033[36mdescription\033[0m: Checks for if the copyright year in the staged files is up to date and displays the files with out-of-date copyright statements. Exits with '0' if successful and with '1' if something is out of date.
 \033[36moptions\033[0m:
@@ -34,31 +34,34 @@ while getopts "auhvkqd:" arg; do
         q) quiet=true;;
         d) diff_hash=${OPTARG};;
         h) print_help; exit;;
+        *) print help; exit 1;;
     esac
 done
 # update & apply copyright when hook config is set
-if `git config --get --local hooks.updateCopyright`; then
+if [ "$(git config --get --type bool --default false hooks.updateCopyright)" = "true" ]; then
     apply=true;update=true;
 fi
 
 # If set, check all files changed since the fork point
 if $forkdiff; then
-    branch=`git rev-parse --abbrev-ref HEAD`
+    branch="$(git rev-parse --abbrev-ref HEAD)"
     remote="$(git config --local --get "branch.$branch.remote" || echo 'origin')"
     source_commit="remotes/$remote/HEAD"
 
     # don't use fork-point for finding fork point (lol)
     # see: https://stackoverflow.com/a/53981615
-    diff_hash=`git merge-base $source_commit $branch`
+    diff_hash="$(git merge-base "$source_commit" "$branch")"
 fi
 
-if [ -n ${diff_hash} ]; then
-    printf "Using the diff: ${diff_hash}\n"
+if [ -n "${diff_hash}" ]; then
+    printf "Using diff: %s\n" "${diff_hash}"
+else
+    diff_hash="HEAD"
 fi
-diff_origin=`git diff ${diff_hash} --cached --name-only`
+diff_origin="$(git diff "${diff_hash}" --cached --name-only)"
 
 # Current year
-year=`date +%Y`
+year="$(date +%Y)"
 
 outdated_copyright=()
 uptodate_copyright=()
@@ -87,7 +90,7 @@ printf "\033[32mDone!\033[0m\n\n"
 if $verbose; then
     if (( ${#notfound_copyright[@]} )); then
         printf "\033[36mCouldn't find the copyright in the following files:\033[0m\n"
-        for i in ${notfound_copyright[@]}; do
+        for i in "${notfound_copyright[@]}"; do
             echo "- $i"
         done
         printf "\n"
@@ -95,7 +98,7 @@ if $verbose; then
 
     if (( ${#uptodate_copyright[@]} )); then
         printf "\033[36mThe copyright statement was already up to date in the following files:\033[0m\n"
-        for i in ${uptodate_copyright[@]}; do
+        for i in "${uptodate_copyright[@]}"; do
             echo "- $i"
         done
         printf "\n"
@@ -109,14 +112,14 @@ fi
 printf \
 "\033[31m==== COPYRIGHT OUT OF DATE ====\033[0m
 \033[36mThe following files need to be updated:\033[0m\n"
-for i in ${outdated_copyright[@]}; do
+for i in "${outdated_copyright[@]}"; do
     echo "- $i"
 done
 
 # If we don't need to update, we early exit.
 if ! $update; then
     printf \
-"\nRun '\033[33mScripts/Copyright/check_year.sh -u\033[0m' to update the copyright statement(s). See '-h' for more info.
+"\nRun '\033[33mscripts/copyright-date/check-copyright.sh -u\033[0m' to update the copyright statement(s). See '-h' for more info.
 Or use '\033[33mgit config --local hooks.updateCopyright true\033[0m' to automatically update copyrights on pre-commit\n"
     exit 1
 fi
@@ -127,7 +130,7 @@ else
     printf "Updating copyrights... "
 fi
 
-for i in ${outdated_copyright[@]}; do
+for i in "${outdated_copyright[@]}"; do
     # Update copyright files...
     sed -E "s/($preamble)([0-9]{4})(-[0-9]{4})?($postamble)/\1\2-$year\4/gi" -i "$i"
     #            .-^-.     .---^----.   .--^---. .-^-.       .-^   ^-.
@@ -136,7 +139,7 @@ for i in ${outdated_copyright[@]}; do
     # If needed, apply the first hunk to the staging environment.
     # This is a reasonable assumption since the copyright header should be at the top.
     if $apply; then
-        git diff -- $i | awk 'second && /^@@/ {exit} /^@@/ {second=1} {print}' | git apply --cached
+        git diff -- "$i" | awk 'second && /^@@/ {exit} /^@@/ {second=1} {print}' | git apply --cached
     fi
 done
 
