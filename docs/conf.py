f5bd0a28c4ace53592ee8f4d5151e5a818876541
Sam Wu <sam.wu2@amd.com>
2023-05-17 16:52:47 -0600

    use doxysphinx instead of exhale due to updated requirements
    rocm-docs-core v0.11.0

--- docs/conf.py
+++ docs/conf.py
@@ -8,24 +8,25 @@ from rocm_docs import ROCmDocs
 
 docs_core = ROCmDocs("hipCUB Documentation")
 docs_core.run_doxygen()
+docs_core.enable_api_reference()
 docs_core.setup()
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
 
-extensions.append('exhale')
-exhale_args = {
-    # These arguments are required
-    "containmentFolder":     "./api",
-    "rootFileName":          "library_root.rst",
-    "rootFileTitle":         "Library API",
-    "doxygenStripFromPath":  "..",
-    # Suggested optional arguments
-    "createTreeView":        True,
-    # TIP: if using the sphinx-bootstrap-theme, you need
-    # "treeViewIsBootstrap": True,
-    "exhaleExecutesDoxygen": False,
-}
-cpp_id_attributes = ["__global__", "__device__", "__host__", "__forceinline__", "static"]
-cpp_paren_attributes = ["__declspec"]
-external_toc_exclude_missing = True
+# extensions.append('exhale')
+# exhale_args = {
+#     # These arguments are required
+#     "containmentFolder":     "./api",
+#     "rootFileName":          "library_root.rst",
+#     "rootFileTitle":         "Library API",
+#     "doxygenStripFromPath":  "..",
+#     # Suggested optional arguments
+#     "createTreeView":        True,
+#     # TIP: if using the sphinx-bootstrap-theme, you need
+#     # "treeViewIsBootstrap": True,
+#     "exhaleExecutesDoxygen": False,
+# }
+# cpp_id_attributes = ["__global__", "__device__", "__host__", "__forceinline__", "static"]
+# cpp_paren_attributes = ["__declspec"]
+# external_toc_exclude_missing = True
