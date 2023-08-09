061750e013747ae25c55703d19c0c2570d714a34
Stanley Tsang <stanley.tsang@amd.com>
2023-08-09 14:07:41 -0400

    Temporarily disable compilation of texture cache iterator on gfx94x

--- test/hipcub/test_hipcub_iterators.cpp
+++ test/hipcub/test_hipcub_iterators.cpp
@@ -335,7 +335,9 @@ TYPED_TEST(HipcubIteratorTests, TestTransform)
 
     g_allocator.DeviceFree(d_data);
 }
-
+#if defined(__gfx940__) || defined(__gfx941__) || defined(__gfx942__)
+#pragma message "Skipping texture cache iterator test compilation for gfx94x as it is not supported."
+#else
 TYPED_TEST(HipcubIteratorTests, TestTexObj)
 {
     int device_id = test_common_utils::obtain_device_from_ctest();
@@ -540,3 +542,6 @@ TYPED_TEST(HipcubIteratorTests, TestTexTransform)
         g_allocator.DeviceFree(d_data);
     }
 }
+#if defined(__gfx940__) || defined(__gfx941__) || defined(__gfx942__)
+#pragma message "Skipping texture cache iterator test compilation for gfx94x as it is not supported."
+#else
