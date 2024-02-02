4d659fb74334fb0b8107517dc6c8295d186c88b9
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2024-02-02 12:58:08 -0700

    Switch back to amdclang and use env vars if set

--- toolchain-linux.cmake
+++ toolchain-linux.cmake
@@ -1,15 +1,19 @@
-#set(CMAKE_MAKE_PROGRAM "nmake.exe")
-#set(CMAKE_GENERATOR "Ninja")
-# Ninja doesn't support platform
-#set(CMAKE_GENERATOR_PLATFORM x64)
 
 if (DEFINED ENV{ROCM_PATH})
   set(rocm_bin "$ENV{ROCM_PATH}/bin")
-  set(llvm_bin "$ENV{ROCM_PATH}/llvm/bin")
 else()
+  set(ROCM_PATH "/opt/rocm" CACHE PATH "Path to the ROCm installation.")
   set(rocm_bin "/opt/rocm/bin")
-  set(llvm_bin "/opt/rocm/llvm/bin")
 endif()
 
-set(CMAKE_CXX_COMPILER "${llvm_bin}/clang++")
-set(CMAKE_C_COMPILER "${llvm_bin}/clang")
+if (NOT DEFINED ENV{CXX})
+  set(CMAKE_CXX_COMPILER "${rocm_bin}/amdclang++")
+else()
+  set(CMAKE_CXX_COMPILER "$ENV{CXX}")
+endif()
+
+if (NOT DEFINED ENV{CC})
+  set(CMAKE_C_COMPILER "${rocm_bin}/amdclang")
+else()
+  set(CMAKE_C_COMPILER "$ENV{CC}")
+endif()
