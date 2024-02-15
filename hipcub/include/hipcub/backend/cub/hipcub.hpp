24c13c00eaed5e04c332d09aaf4e5d5db7522696
Lőrinc Serfőző <lorinc@streamhpc.com>
2024-02-15 09:12:26 +0000

    Added hipcub::tuple

--- hipcub/include/hipcub/backend/cub/hipcub.hpp
+++ hipcub/include/hipcub/backend/cub/hipcub.hpp
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Copyright (c) 2010-2011, Duane Merrill.  All rights reserved.
  * Copyright (c) 2011-2018, NVIDIA CORPORATION.  All rights reserved.
- * Modifications Copyright (c) 2017-2023, Advanced Micro Devices, Inc.  All rights reserved.
+ * Modifications Copyright (c) 2017-2024, Advanced Micro Devices, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -103,6 +103,7 @@
 #include <cub/warp/warp_store.cuh>
 
 // Util
+#include "tuple.hpp"
 #include <cub/util_debug.cuh>
 #include <cub/util_device.cuh>
 #include <cub/util_macro.cuh>
