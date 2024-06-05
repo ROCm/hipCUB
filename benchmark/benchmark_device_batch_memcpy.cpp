fc4634917804934df4e1b826819488833c14e82e
NguyenNhuDi <dinguyennhu@gmail.com>
2024-06-05 11:16:24 -0600

    reformated key:field for benchmark_device_memcpy

--- benchmark/benchmark_device_batch_memcpy.cpp
+++ benchmark/benchmark_device_batch_memcpy.cpp
@@ -337,16 +337,31 @@ void run_benchmark(benchmark::State& state,
     HIP_CHECK(hipFree(d_temp_storage));
 }
 
-#define CREATE_BENCHMARK(item_size, item_alignment, size_type, num_tlev, num_wlev, num_blev)     \
-    benchmark::RegisterBenchmark(                                                                \
-        "{lvl:device,item_size:" #item_size ",item_alignment:" #item_alignment                   \
-        ",size_type:" #size_type ",algo:batch_memcpy,num_tlev:" #num_tlev ",num_wlev:" #num_wlev \
-        ",num_blev:" #num_blev ",cfg:default_config}",                                           \
-        [=](benchmark::State& state)                                                             \
-        {                                                                                        \
-            run_benchmark<benchmark_utils::custom_aligned_type<item_size, item_alignment>,       \
-                          size_type>(state, stream, num_tlev, num_wlev, num_blev);               \
-        })
+// #define CREATE_BENCHMARK(item_size, item_alignment, size_type, num_tlev, num_wlev, num_blev)     \
+    // benchmark::RegisterBenchmark(                                                                \
+    //     "{lvl:device,item_size:" #item_size ",item_alignment:" #item_alignment                   \
+    //     ",size_type:" #size_type ",algo:batch_memcpy,num_tlev:" #num_tlev ",num_wlev:" #num_wlev \
+    //     ",num_blev:" #num_blev ",cfg:default_config}",                                           \
+    //     [=](benchmark::State& state)                                                             \
+    //     {                                                                                        \
+    //         run_benchmark<benchmark_utils::custom_aligned_type<item_size, item_alignment>,       \
+    //                       size_type>(state, stream, num_tlev, num_wlev, num_blev);               \
+    //     })
+
+#define CREATE_BENCHMARK(IS, IA, T, num_tlev, num_wlev, num_blev)                   \
+    benchmark::RegisterBenchmark(                                                   \
+        (std::string("device_batch_memcpy<Datatype:" #T                             \
+            ",Item Size:" #IS                                                       \
+            ",Item Alignment:" #IA                                                  \
+            ",Number of Tlev:" #num_tlev                                            \
+            ",Number of Wlev:" #num_wlev                                            \
+            ",Number of Blev:" #num_blev ">.")                                      \
+        ).c_str(),                                                                  \
+        [=](benchmark::State& state){                                               \
+            run_benchmark<benchmark_utils::custom_aligned_type<IS, IA>,             \
+                          T>(state, stream, num_tlev, num_wlev, num_blev);  \
+        }                                                                           \
+    )
 
 #define BENCHMARK_TYPE(item_size, item_alignment)                            \
     CREATE_BENCHMARK(item_size, item_alignment, uint32_t, 100000, 0, 0),     \
