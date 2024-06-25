fb30fd1d301735e94d9f6d8880ce33f85b6f00ef
NguyenNhuDi <Zee.nguyen@amd.com>
2024-06-25 12:08:31 -0600

    added print of benchmark_warp_load at start

--- benchmark/benchmark_warp_load.cpp
+++ benchmark/benchmark_warp_load.cpp
@@ -146,6 +146,8 @@ int main(int argc, char* argv[])
     int             device_id = 0;
     HIP_CHECK(hipGetDevice(&device_id));
     HIP_CHECK(hipGetDeviceProperties(&devProp, device_id));
+    
+    std::cout << "benchmark_warp_load" << std::endl;
     std::cout << "[HIP] Device name: " << devProp.name << std::endl;
 
     // Add benchmarks
