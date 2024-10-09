56f52a277c2aa299832f07643a62070ba8cbda54
Gary Deng <gary.deng@amd.com>
2024-10-09 13:06:00 -0600

    add xnack build targets and asan settings

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -15,6 +15,9 @@ def runCompileCommand(platform, project, jobName, settings)
 
     def getRocPRIM = auxiliary.getLibrary('rocPRIM', platform.jenkinsLabel, null, sameOrg)
 
+    if (settings.addressSanitizer){
+        amdgpuTargets = '-DAMDGPU_TARGETS=\$gfx_arch:xnack' 
+    }
     def command = """#!/usr/bin/env bash
                 set -x
                 ${getRocPRIM}
@@ -35,12 +38,23 @@ def runTestCommand (platform, project, settings)
 
     def testCommand = "ctest --output-on-failure --verbose --timeout 900"
     def LD_PATH = 'export LD_LIBRARY_PATH=/opt/rocm/lib/'
+    def asanArgs = ''
     if (settings.addressSanitizer)
     {
         LD_PATH = """
                     export ASAN_LIB_PATH=\$(/opt/rocm/llvm/bin/clang -print-file-name=libclang_rt.asan-x86_64.so)
                     export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$(dirname "\${ASAN_LIB_PATH}")
                   """
+        asanArgs = """
+        export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/rocm/llvm/lib/clang/18/lib/linux
+        export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/rocm/lib/asan
+        export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/rocm/libexec/rocm_smi
+        export ASAN_SYMBOLIZER_PATH=/opt/rocm/llvm/bin/llvm-symbolizer
+        export PATH=/opt/rocm/llvm/bin/:\$PATH
+        export PATH=/opt/rocm/:\$PATH
+        export HSA_XNACK=1
+        export ASAN_OPTIONS=detect_leaks=0
+        """   
     }
 
     def command = """#!/usr/bin/env bash
@@ -48,6 +62,7 @@ def runTestCommand (platform, project, settings)
                 cd ${project.paths.project_build_prefix}
                 cd ${project.testDirectory}
                 ${LD_PATH}
+                ${asanArgs}
                 ${sudo} ${testCommand}
             """
 
