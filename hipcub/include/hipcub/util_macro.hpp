b90c0f2d62712b1b67ca1e7a4e5ff747437ac69e
Nick Breed <nick@streamhpc.com>
2025-01-20 14:22:50 +0000

    Update copyright dates

--- hipcub/include/hipcub/util_macro.hpp
+++ hipcub/include/hipcub/util_macro.hpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Modifications Copyright (c) 2024, Advanced Micro Devices, Inc.  All rights reserved.
+ * Modifications Copyright (c) 2024-2025, Advanced Micro Devices, Inc.  All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
