6f94b0272c459f8238ef861dd32ae01a6cc8ef56
Cenxuan <104080607+cenxuantian@users.noreply.github.com>
2025-08-21 21:00:35 +0000

    [rocm-libraries] ROCm/rocm-libraries#859 (commit 728fbfb)
    [hipCUB][CCCL2.8 parity] Add-for-each-in-extents-to-hipcub
     (#859)
    
    ## Background
    
    Investigate how to add the new `ForEachInExtents` added to CUB in
    https://github.com/NVIDIA/cccl/commit/2e6ed5932#diff-2dadfd76ed1b6f638a38def9d9d294c7a9f3b88ebc81215242751912ab8216c8.
    They are also adding an `fast_modulo_division` which might be
    interesting.
    
    ## Notes for the Reviewer
    
    ### What has been done?
    
    * `::hipcub::extents` has been implemented, with limited parity to
    C++23's `std::extents`. Only **static extents** is supported; **dynamic
    extents** is not.
    * Helper structs/templates have been created to perform computations on
    `::hipcub::extents` only when the backend is **rocPRIM**. For the
    **CUDA** backend, similar functionality exists.
    * The `ForEachInExtents` interface has been implemented, based on
    `rocprim::transform`.
    * Tests for `ForEachInExtents` have been added and are compatible with
    both the CUDA and rocPRIM backends.
    
    ### About `Fast_div_mod`
    
    * Which is implemented in CUB, but is not implemented in this MR, as it
    is unnecessary when only static extents is used. It can be added later
    if we decide to support the full functionality of `std::extents`.
    
    ### About Full Functionality of `std::extents`
    
    * A better approach might be to enable compiler support for the
    `<mdspan>` header as an extension or implement it in the comming
    `libhipcxx`.
    * CUDA appears to be including all related `<mdspan>` headers and
    enabling support under C++17, then introducing a macro like
    `__cccl_lib_mdspan` to check if the extension is supported in a
    particular compiler version.
    * Currently, only essential functionality has been implemented.

--- hipcub/include/hipcub/backend/rocprim/device/device_for.hpp
+++ hipcub/include/hipcub/backend/rocprim/device/device_for.hpp
@@ -34,6 +34,7 @@
 #include "../iterator/counting_input_iterator.hpp"
 #include "../iterator/discard_output_iterator.hpp"
 #include "../thread/thread_operators.hpp"
+#include "../util_mdspan.hpp"
 
 #include <rocprim/device/device_transform.hpp> // IWYU pragma: export
 
@@ -61,6 +62,50 @@ struct OpWrapper
     }
 };
 } // namespace bulk
+
+namespace for_each_in_extents
+{
+template<class OpT, class IndexType, class ExtentsIndexType, size_t... Extents>
+struct OpWrapper
+{
+    using extents_type = ::hipcub::extents<ExtentsIndexType, Extents...>;
+    OpT op;
+
+    template<class IndexSeqType>
+    struct OpDispatcher
+    {};
+
+    template<size_t... Rank>
+    struct OpDispatcher<std::index_sequence<Rank...>>
+    {
+        __device__ __host__ __forceinline__
+        void operator()(IndexType idx, OpT op) const
+        {
+            op(idx, OpWrapper::coordinate_at<Rank>(idx)...);
+        }
+    };
+
+    template<size_t Rank>
+        __device__ __host__ __forceinline__
+    static auto coordinate_at(typename extents_type::index_type index)
+    {
+        using extent_index_type = typename extents_type::index_type;
+        return static_cast<extent_index_type>(
+            (index / extents_sub_size<Rank + 1, extents_type>::value)
+            % extents_type::static_extent(Rank));
+    }
+
+    __device__ __host__ __forceinline__
+    IndexType
+        operator()(IndexType idx)
+    {
+        using seq_type = std::make_index_sequence<extents_type::rank()>;
+        OpDispatcher<seq_type>{}(idx, op);
+        return 0;
+    }
+};
+} // namespace for_each_in_extents
+
 } // namespace detail
 
 struct DeviceFor
@@ -251,6 +296,80 @@ HIPCUB_RUNTIME_FUNCTION
         }
 
         return Bulk(shape, op, stream);
+    }
+
+    /**
+     * \brief Iterate through a multi-dimensional extents.
+     * 
+     * \tparam IndexType The index type to create the extents.
+     * \tparam Extents The sizes of each extent.
+     * \tparam OpT The function that will be applied for each iteration. The function should be callable
+     * with arguments (IndexType, ExtentsIndexType...extents_idx). The size of extents_idx... should be 
+     * the same as the rank of the the multi-dimensional extents.
+     */
+    template<class IndexType, size_t... Extents, typename OpT>
+    HIPCUB_RUNTIME_FUNCTION
+    static hipError_t ForEachInExtents(void*   d_temp_storage,
+                                       size_t& temp_storage_bytes,
+                                       const ::hipcub::extents<IndexType, Extents...>& extents,
+                                       OpT                                             op,
+                                       hipStream_t                                     stream = {})
+    {
+        // Index overlapping is checked by the `in_range` in the implementation in `::hipcub::extents`.
+        // TODO: it's better to add a check here:
+        // `static_assert(std::is_invocable::<OpT, IndexType, decltype(static_cast<extents_index_type>(Extents))...>::value,"check invocable.")`
+        // But the `std::is_invocalble` does not work well here. Checked on a host environment with clang 19.1.0, and it works there. Since this
+        // could be a compiler bug and invocability is also checked by the compiler. So, a "TODO" is left here.
+        if(d_temp_storage == nullptr)
+        {
+            temp_storage_bytes = 1;
+            return hipSuccess;
+        }
+        return ForEachInExtents(extents, op, stream);
+    }
+
+    /**
+     * \brief Iterate through a multi-dimensional extents.
+     * 
+     * \tparam IndexType The index type to create the extents.
+     * \tparam Extents The sizes of each extent.
+     * \tparam OpT The function that will be applied for each iteration. The function should be callable
+     * with arguments (IndexType, ExtentsIndexType...extents_idx). The size of extents_idx... should be 
+     * the same as the rank of the the multi-dimensional extents.
+     */
+    template<class IndexType, size_t... Extents, typename OpT>
+    HIPCUB_RUNTIME_FUNCTION
+    static hipError_t ForEachInExtents(const ::hipcub::extents<IndexType, Extents...>&,
+                                       OpT         op,
+                                       hipStream_t stream = {})
+    {
+        using ext_type       = ::hipcub::extents<IndexType, Extents...>;
+        using ext_index_type = typename ext_type::index_type;
+        using wrapped_op_type
+            = detail::for_each_in_extents::OpWrapper<OpT, IndexType, ext_index_type, Extents...>;
+
+        // CountingInputIterator only holds the index, not the data.
+        using InputIterator = typename hipcub::CountingInputIterator<IndexType>;
+        // We don't actually need the output, so we use DiscardOutputIterator here as a placeholder.
+        using OutputIterator = typename hipcub::DiscardOutputIterator<IndexType>;
+
+        // How many times rocprim::transform will iterate.
+        constexpr auto ext_size = ::hipcub::extents_size<ext_type>::value;
+
+        InputIterator  input(IndexType(0)); // Initialize the input iterator, starting from 0.
+        OutputIterator output;
+
+        // `ForEachInExtents` only iterates over the extents on device and does not guarantee ordering.
+        // We only need to invoke `$op` `$ext_size` times. Therefore, `rocprim::transform` is suitable.
+        // In `ForEachInExtents`, the data isn’t necessarily needed, but even if it's needed, it may be embedded
+        // in `OpT`. We only need to iterate exactly `$ext_size` times. Therefore, we use a `CountingInputIterator`,
+        // which provides only the index, and a `DiscardOutputIterator` for output.
+        return rocprim::transform(input,
+                                  output,
+                                  ext_size,
+                                  wrapped_op_type{op},
+                                  stream,
+                                  HIPCUB_DETAIL_DEBUG_SYNC_VALUE);
     }
 };
 
